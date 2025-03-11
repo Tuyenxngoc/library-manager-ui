ec614329fd0906a3be093a26ec9ff430e1a30634
tuyenxngoc <tuyenn9448@gmail.com>
2025-03-11 17:23:09 +0700

    refactor: move RequireAuth utility to routes directory and update import path in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import DefaultLayout from './layouts/DefaultLayout';
 
-import RequireAuth from './utils/RequireAuth';
+import RequireAuth from './routes/RequireAuth';
 import { ROLES } from './common/roleConstants';
 
 import Home from './pages/Home';
